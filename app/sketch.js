2a1024af1f17938d8b52d277eb55a6752da71409
philFernandez <norcal618@gmail.com>
2021-05-04 01:54:26 -0700

    peru bg

--- app/sketch.js
+++ app/sketch.js
@@ -1,7 +1,7 @@
 const groundHeight = 60;
 let ground, player;
 function setup() {
-    createCanvas(window.innerWidth, window.innerHeight);
+    createCanvas(window.innerWidth, window.innerHeight - 1);
     ground = new Ground(groundHeight, "forestgreen");
     player = new Player("blue");
 }
