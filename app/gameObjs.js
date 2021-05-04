0de1f223490f2ea9811e4b2ebdc4ef219a461fb4
philFernandez <norcal618@gmail.com>
2021-05-04 01:44:49 -0700

    player

--- app/gameObjs.js
+++ app/gameObjs.js
@@ -6,12 +6,26 @@ class Ground {
 
     show() {
         // x, y, w, h
-        rect(0, height - this.groundHeight, width, this.groundHeight);
         noStroke();
         fill(this.color);
+        rect(0, height - this.groundHeight, width, this.groundHeight);
     }
 
     getGroundHeight() {
         return this.groundHeight;
+    }
+}
+
+class Player {
+    constructor(color) {
+        this.color = color;
+        this.x = 220;
+        this.diameter = 70;
+        this.y = height - groundHeight - this.diameter / 2;
+    }
+
+    show() {
+        fill(this.color);
+        circle(this.x, this.y, this.diameter);
     }
 }
--- app/sketch.js
+++ app/sketch.js
@@ -1,10 +1,13 @@
-let ground;
+const groundHeight = 60;
+let ground, player;
 function setup() {
     createCanvas(window.innerWidth, window.innerHeight);
-    ground = new Ground(60, "forestgreen");
+    ground = new Ground(groundHeight, "forestgreen");
+    player = new Player("blue");
 }
 
 function draw() {
     background("skyblue");
     ground.show();
+    player.show();
 }
